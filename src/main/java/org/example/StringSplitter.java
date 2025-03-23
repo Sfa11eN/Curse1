f7f213d6b5890b4ffe5fde305fe2387ca4300624
SunSiN <sfallenn@gmail.com>
2025-03-23 20:11:32 +0300

    Final commit

--- src/main/java/org/example/StringSplitter.java
+++ src/main/java/org/example/StringSplitter.java
@@ -5,22 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringSplitter {
-        public static List<String> splitString(String input ,int partSize){
-                List<String> parts=new ArrayList<>();
-                int length=input.length();
+    public static List<String> splitString(String input, int partSize) {
+        List<String> parts = new ArrayList<>();
+        int length = input.length();
 
-                for(int i =0;i<length;i+=partSize){
-                        parts.add(input.substring(i,Math.min(length,i+partSize)));
-                }
-                return parts;
+        for (int i = 0; i < length; i += partSize) {
+            parts.add(input.substring(i, Math.min(length, i + partSize)));
         }
-        public static void main(String[] args) {
-                String input="HelloWorldJavaProgramming";
-                int partSize=5;
+        return parts;
+    }
 
-                List<String> result=splitString(input,partSize);
-                System.out.println(result);
-        }
+    public static void main(String[] args) {
+        String input = "HelloWorldJavaProgramming";
+        int partSize = 5;
 
-        }
+        List<String> result = splitString(input, partSize);
+        System.out.println(result);
+    }
+
+}
 
